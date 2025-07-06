6d004b634233c56020529eca9683d18bbb781c33
Worty <6840978+Worty@users.noreply.github.com>
2025-07-06 23:08:32 +0200

    test: Metrics test

---
+++ exporter_test.go
@@ -0,0 +1,32 @@
+package resticrepoexporter
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestMetrics(t *testing.T) {
+	exporter := Exporter{
+		repoPath:              "testdata",
+		scrapeIntervalSeconds: 1,
+	}
+
+	os.Setenv("RESTIC_PASSWORD_locked-repo", "abc123")
+	os.Setenv("RESTIC_PASSWORD_repo-with-tags", "abc123")
+
+	ctx, cancel := context.WithTimeout(t.Context(), 5*time.Second)
+	defer cancel()
+
+	exporter.Scan(ctx)
+
+	got := testutil.ToFloat64(numSnapshots.WithLabelValues("locked-repo", "Wortys-Thinkpad", ""))
+	want := 2.0
+
+	if want != got {
+		t.Errorf("unexpected number of snapshots: got %f, want %f", got, want)
+	}
+}
